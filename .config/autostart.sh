7e557faec272f9c8ff1139427d6ce5ccde3e8622
Laszlo Szurok <szuroklaszlo97@gmail.com>
2020-05-18 18:24:28 +0200

    updating the path to the suspend script

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wal -R -o '/home/pulzar/.config/dunst/dunst_with_pywal.sh'
+wal -R -o '$HOME/.config/dunst/dunst_with_pywal.sh'
 setxkbmap -layout hu,us -option 'grp:alt_space_toggle'
 numlockx &
 wmname LG3D
 lxpolkit &
 light-locker --lock-after-screensaver=120 &
-/home/pulzar/bin/suspend_after_screensaver &
+$HOME/scripts/misc/suspend_after_screensaver &
 dwmblocks

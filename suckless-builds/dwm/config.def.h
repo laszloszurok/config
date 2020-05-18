1547adf9a020657c970d685d2538292ace53a350
Laszlo Szurok <szuroklaszlo97@gmail.com>
2020-05-18 18:55:09 +0200

    adding include line for pywal colors into dwm/config.def.h (commented out)

--- suckless-builds/dwm/config.def.h
+++ suckless-builds/dwm/config.def.h
@@ -17,6 +17,9 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+// #include "/home/pulzar/.cache/wal/colors-wal-dwm.h"
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },

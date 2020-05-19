68f423b96b71a553d6bfc28e5101ff5ebc53b25b
Laszlo Szurok <szuroklaszlo97@gmail.com>
2020-05-19 17:04:17 +0200

    rearranging color definitions in dwm/config.def.h

--- suckless-builds/dwm/config.def.h
+++ suckless-builds/dwm/config.def.h
@@ -12,13 +12,14 @@ static const int horizpadbar        = 2;        /* horizontal padding for status
 static const int vertpadbar         = 5;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "FontAwesome:size=12", "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=12";
+
+// #include "/home/pulzar/.cache/wal/colors-wal-dwm.h"
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-
-// #include "/home/pulzar/.cache/wal/colors-wal-dwm.h"
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */

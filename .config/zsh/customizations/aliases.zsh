efe6c37f706f6ebe6eccc98053ab1b65defbbc65
László Szurok <szuroklaszlo97@gmail.com>
2020-07-17 15:29:02 +0200

    refactored .zshrc, parts placed in separate scripts

---
+++ .config/zsh/customizations/aliases.zsh
@@ -0,0 +1,6 @@
+alias vf=vifmrun # vifmrun is a custom script for vifm with image preview (~/scripts/misc/vifmrun)
+alias vim=nvim
+alias ll="ls -lhF"
+alias la="ls -lahF"
+alias ls="ls --color=auto"
+alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME' # alias for managing files in the home folder with git
---
+++ .config/zsh/customizations/prompt.zsh
@@ -0,0 +1,21 @@
+# Show git branch in right prompt if inside a git dir
+setopt prompt_subst
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' actionformats \
+    '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+zstyle ':vcs_info:*' formats       \
+    '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+zstyle ':vcs_info:*' enable git cvs svn
+vcs_info_wrapper() {
+  vcs_info
+  if [ -n "$vcs_info_msg_0_" ]; then
+    echo "%{$fg[grey]%}${vcs_info_msg_0_}%{$reset_color%}$del"
+  else
+    echo "  %m    %n"
+  fi
+}
+
+RPROMPT=$'$(vcs_info_wrapper)'
+PS1="%{$fg[green]%}cwd: %{$fg[yellow]%}%~"$'\n'"%{$reset_color%}%{$reset_color%} "
+# RPS1="  %m    %n"

1cfbe81234fb0327ed653895dc7844d06172035a
László Szurok <szuroklaszlo97@gmail.com>
2020-07-19 14:35:54 +0200

    rearranged status scripts

--- suckless-builds/dwmblocks/blocks.h
+++ suckless-builds/dwmblocks/blocks.h
@@ -1,21 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// │
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/	   /*Update Signal*/
-    { "  ",     "~/scripts/status/wifinetwork",      10,       0 },
-
-    { "│ ",     "~/scripts/status/disk",             60,       0 },
-
-    { "│ ",     "~/scripts/status/volume",	          0,	  10 },
-
-    { "│ ",     "~/scripts/status/brightness",        0,	  11 },
-	
-    { "│ ",     "~/scripts/status/battery",           5,	   0 },
-    
-    { "│ ",     "~/scripts/status/kblayout",	      0,	  12 },
-    
-    { "│ ",     "~/scripts/status/lightlockerstatus", 0,	  14 },
-
-    { "│ ",     "~/scripts/status/clock",	          5,	   0 },
+    { "  ",    "~/scripts/status/wifinetwork",          20,   15 },
+    { "  ",     "~/scripts/status/disk",                60,   16 },
+    { "  ",     "~/scripts/status/lightlockerstatus",   0,    14 },
+    { "  ",     "~/scripts/status/kblayout",            0,    12 },
+    { "  ",     "~/scripts/status/volume",              0,    10 },
+    { "  ",     "~/scripts/status/brightness",          0,    11 },
+    { "  ",     "~/scripts/status/battery",             5,    16 },
+    { "  ",     "~/scripts/status/clock",               5,    13 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

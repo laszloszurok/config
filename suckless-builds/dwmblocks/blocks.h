236d5a1134fa44c9835fe01d5b06d25ee9923f5e
László Szurok <szuroklaszlo97@gmail.com>
2020-07-19 17:58:16 +0200

    added new status indicators

--- suckless-builds/dwmblocks/blocks.h
+++ suckless-builds/dwmblocks/blocks.h
@@ -2,12 +2,14 @@
 // │
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/	   /*Update Signal*/
-    { "  ",    "~/scripts/status/wifinetwork",          20,   15 },
+    { "  ",     "~/scripts/status/wifinetwork",         20,   15 },
+    { "  ",     "~/scripts/status/cpu",                 0,    18 },
+    { "  ",     "~/scripts/status/memory",              0,    19 },
     { "  ",     "~/scripts/status/disk",                60,   16 },
     { "  ",     "~/scripts/status/lightlockerstatus",   0,    14 },
     { "  ",     "~/scripts/status/kblayout",            0,    12 },
     { "  ",     "~/scripts/status/volume",              0,    10 },
-    { "  ",     "~/scripts/status/brightness",          0,    11 },
+    /* { "  ",     "~/scripts/status/brightness",          0,    11 }, */
     { "  ",     "~/scripts/status/battery",             5,    17 },
     { "  ",     "~/scripts/status/clock",               5,    13 },
 };

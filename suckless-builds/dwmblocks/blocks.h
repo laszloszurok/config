be665dadbc0b48b723d6fc6d0d64c2624c16c854
László Szurok <szuroklaszlo97@gmail.com>
2020-07-22 11:01:01 +0200

    changed update interval of status indicators

--- suckless-builds/dwmblocks/blocks.h
+++ suckless-builds/dwmblocks/blocks.h
@@ -2,16 +2,16 @@
 // │
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/	   /*Update Signal*/
-    { "  ",     "~/scripts/status/wifinetwork",         20,   15 },
-    { "  ",     "~/scripts/status/cpu",                 0,    18 },
-    { "  ",     "~/scripts/status/memory",              0,    19 },
-    { "  ",     "~/scripts/status/disk",                60,   16 },
-    { "  ",     "~/scripts/status/lightlockerstatus",   0,    14 },
-    { "  ",     "~/scripts/status/kblayout",            0,    12 },
-    { "  ",     "~/scripts/status/volume",              0,    10 },
+    { "  ",      "~/scripts/status/wifinetwork",         0,    15 },
+    { "   ",     "~/scripts/status/cpu",                 0,    18 },
+    { "   ",     "~/scripts/status/memory",              0,    19 },
+    { "   ",     "~/scripts/status/disk",                0,    16 },
+    { "   ",     "~/scripts/status/lightlockerstatus",   0,    14 },
+    { "   ",     "~/scripts/status/kblayout",            0,    12 },
+    { "   ",     "~/scripts/status/volume",              0,    10 },
     /* { "  ",     "~/scripts/status/brightness",          0,    11 }, */
-    { "  ",     "~/scripts/status/battery",             5,    17 },
-    { "  ",     "~/scripts/status/clock",               5,    13 },
+    { "   ",     "~/scripts/status/battery",             5,    17 },
+    { "   ",     "~/scripts/status/clock",               5,    13 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

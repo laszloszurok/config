f2cd5b4110f427f149d54c35a364005af1057441
László Szurok <szuroklaszlo97@gmail.com>
2020-07-28 10:12:18 +0200

    fixed color include

--- suckless-builds/dwm/config.def.h
+++ suckless-builds/dwm/config.def.h
@@ -16,18 +16,18 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FontAwesome:size=12", "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=12";
 
-#include "/home/pulzar/.cache/wal/colors-wal-dwm.h"
+/* #include "/home/pulzar/.cache/wal/colors-wal-dwm.h" */
 
-/* static const char col_gray1[]       = "#222222"; */
-/* static const char col_gray2[]       = "#444444"; */
-/* static const char col_gray3[]       = "#bbbbbb"; */
-/* static const char col_gray4[]       = "#eeeeee"; */
-/* static const char col_cyan[]        = "#005577"; */
-/* static const char *colors[][3]      = { */
-/* 	/1*               fg         bg         border   *1/ */
-/* 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */
-/* 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
-/* }; */
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
 
 /* tagging */
 static const char *tags[] = { "", "", "", "", "", "",  "", "", "" };

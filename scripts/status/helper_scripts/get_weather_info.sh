09cf8f45b6e21a9d0500cd8ae13952590517370e
László Szurok <szuroklaszlo97@gmail.com>
2020-07-28 11:14:09 +0200

    new dir for helper scripts

---
+++ scripts/status/helper_scripts/get_weather_info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ ! -d "$HOME/.cache/weather" ] && mkdir $HOME/.cache/weather
+
+getforecast() { curl -sf "wttr.in" > "$HOME/.cache/weather/weatherinfo.txt" || exit 1 ;}
+
+getforecast
+
+less -Srf "$HOME/.cache/weather/weatherinfo.txt"

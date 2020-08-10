628d38a1cccd5d98adad1da83c132c9068496039
László Szurok <szuroklaszlo97@gmail.com>
2020-08-10 21:26:10 +0200

    swapping capslock and escape functionality at startup

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wal -R -o "$HOME/.config/dunst/dunst_with_pywal.sh"
-setxkbmap -layout us,hu -option 'grp:alt_space_toggle'
+setxkbmap -layout us,hu -option 'grp:alt_space_toggle' -option 'caps:swapescape'
 numlockx &
 wmname LG3D
 lxpolkit &

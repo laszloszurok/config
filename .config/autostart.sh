446463e80962a15d50a44afeda50762aaa14deb9
László Szurok <szuroklaszlo97@gmail.com>
2020-08-15 20:51:31 +0200

    added picom to autostart.sh

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wal -R -o "$HOME/.config/dunst/dunst_with_pywal.sh"
 setxkbmap -layout us,hu -option 'grp:alt_space_toggle' -option 'caps:swapescape'
+picom &
 numlockx &
 wmname LG3D
 lxpolkit &

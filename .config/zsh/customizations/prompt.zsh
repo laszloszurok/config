688eb3ee3bb0673390c4c9087e6acc61bd2cee03
László Szurok <szuroklaszlo97@gmail.com>
2020-08-15 22:25:28 +0200

    changed colors of prompt

--- .config/zsh/customizations/prompt.zsh
+++ .config/zsh/customizations/prompt.zsh
@@ -2,9 +2,9 @@
 setopt prompt_subst
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' actionformats \
-    '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+    '%F{5}[on branch:%F{3}-%F{5}%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
 zstyle ':vcs_info:*' formats       \
-    '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
+    '%F{5}[on branch%F{3} %F{5}%F{2}%b%F{5}]%f '
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
 zstyle ':vcs_info:*' enable git cvs svn
 vcs_info_wrapper() {
@@ -12,10 +12,10 @@ vcs_info_wrapper() {
   if [ -n "$vcs_info_msg_0_" ]; then
     echo "%{$fg[grey]%}${vcs_info_msg_0_}%{$reset_color%}$del"
   else
-    echo "  %m    %n"
+    echo "  %{$fg[green]%}%m%{$reset_color%}    %{$fg[green]%}%n%{$reset_color%}"
   fi
 }
 
 RPROMPT=$'$(vcs_info_wrapper)'
-PS1="%{$fg[green]%}cwd: %{$fg[yellow]%}%~"$'\n'"%{$reset_color%}%{$reset_color%} "
+PS1="%{$fg[green]%}cwd: %~"$'\n'"%{$reset_color%} "
 # RPS1="  %m    %n"

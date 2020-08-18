ca2435d3d00457836c16bad8b6c73bff08ffd56a
László Szurok <szuroklaszlo97@gmail.com>
2020-08-18 21:04:57 +0200

    changed config alias to cfg

--- .config/zsh/customizations/aliases.zsh
+++ .config/zsh/customizations/aliases.zsh
@@ -3,4 +3,4 @@ alias nv=nvim
 alias ll="ls -lhF"
 alias la="ls -lahF"
 alias ls="ls --color=auto"
-alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME' # alias for managing files in the home folder with git
+alias cfg='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME' # alias for managing files in the home folder with git

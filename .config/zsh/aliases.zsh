939afb5793fac9b508502f5db1a77d01076ca7af
László Szurok <szuroklaszlo97@gmail.com>
2020-08-19 18:43:31 +0200

    added new aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,6 +3,8 @@ alias nv=nvim
 alias ll="ls -lhF"
 alias la="ls -lahF"
 alias ls="ls --color=auto"
+alias lh="ls -d .*"
+alias llh="ls -ld .*"
 alias cfg='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME' # alias for managing files in the home folder with git
 alias ..='cd ..' 
 alias ...='cd ../..'

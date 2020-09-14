e9ea1e59bcae6212c77e9c64b422b5d8a759a2c0
László Szurok <szuroklaszlo97@gmail.com>
2020-09-14 18:12:31 +0200

    readding border after switching from monocle layout

--- suckless-builds/dwm/dwm.c
+++ suckless-builds/dwm/dwm.c
@@ -1311,8 +1311,10 @@ monocle(Monitor *m)
 	if (n > 0) /* override layout symbol */
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "│  [%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
+        int old_bw = c->bw;
         c->bw = 0;
 		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+        c->bw = old_bw;
     }
 }
 

cb59be8a2e4a600f58a6fcc6792d8e08b74a6b49
László Szurok <szuroklaszlo97@gmail.com>
2020-09-24 14:33:36 +0200

    removed autostart.sh

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-wal -R -o "$HOME/.config/dunst/dunst_with_pywal.sh"
-setxkbmap -layout us,hu -option 'grp:alt_space_toggle' -option 'caps:swapescape'
-picom &
-numlockx &
-wmname LG3D
-lxpolkit &
-$HOME/scripts/misc/inactivity &
-$HOME/scripts/status/helper_scripts/reload_newsboat &
-dwmblocks
+# wal -R -o "$HOME/.config/dunst/dunst_with_pywal.sh"
+# setxkbmap -layout us,hu -option 'grp:alt_space_toggle' -option 'caps:swapescape'
+# picom &
+# numlockx &
+# wmname LG3D
+# lxpolkit &
+# $HOME/scripts/misc/inactivity &
+# $HOME/scripts/status/helper_scripts/reload_newsboat &
+# dwmblocks

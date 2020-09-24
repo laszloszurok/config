f2de738d8fb6f2e0c2483ded7a5011b8a82097f2
László Szurok <szuroklaszlo97@gmail.com>
2020-09-24 17:21:48 +0200

    sourcing colors from custom header file

--- suckless-builds/dmenu/config.def.h
+++ suckless-builds/dmenu/config.def.h
@@ -9,14 +9,7 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-/* #include "/home/pulzar/.cache/wal/colors-wal-dmenu.h" */
-
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+#include "../color-source/dmenu-colors.h"
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;

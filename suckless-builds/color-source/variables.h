1efb71fa9c7643a5511dd4c9e447c438000f75be
László Szurok <szuroklaszlo97@gmail.com>
2020-09-25 10:19:09 +0200

    changed border color of unfocused windows

--- suckless-builds/color-source/variables.h
+++ suckless-builds/color-source/variables.h
@@ -1,6 +1,6 @@
 static const char norm_fg[] = "#e0dbd2";
 static const char norm_bg[] = "#191b28";
-static const char norm_border[] = "#9c9993";
+static const char norm_border[] = "#333";
 
 static const char sel_fg[] = "#e0dbd2";
 static const char sel_bg[] = "#563d7c";

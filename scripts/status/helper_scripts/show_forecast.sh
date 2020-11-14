a80facae082e967f5abd85fdebc7ba1412585615
László Szurok <szuroklaszlo97@gmail.com>
2020-11-14 16:43:41 +0100

    refactoring weather script

--- scripts/status/helper_scripts/show_forecast.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[ ! -d "$HOME/.cache/weather" ] && mkdir $HOME/.cache/weather
-
-getforecast() { curl -sf "wttr.in" > "$HOME/.cache/weather/weatherinfo" || exit 1 ;}
-
-getforecast
-
-less -Srf "$HOME/.cache/weather/weatherinfo"

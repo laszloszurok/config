a806db851ff1f95a067310f5b142d58ac63a8159
László Szurok <szuroklaszlo97@gmail.com>
2020-12-07 15:32:20 +0100

    renamed file

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,10 +1,9 @@
-alias vf='vifmrun .' # vifmrun is a custom script for vifm with image preview (~/scripts/misc/vifmrun)
 alias nv=nvim
-alias ll="ls -lhF"
-alias la="ls -lahF"
-alias ls="ls --color=auto"
-alias lh="ls -d .*"
-alias llh="ls -ld .*"
+alias ll="ls -lhF --group-directories-first"
+alias la="ls -lAhF --group-directories-first"
+alias ls="ls --color=auto --group-directories-first"
+alias lh="ls -d .* --group-directories-first"
+alias llh="ls -ld .* --group-directories-first"
 alias cfg='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME' # alias for managing files in the home folder with git
 alias ..='cd ..' 
 alias ...='cd ../..'
@@ -12,5 +11,3 @@ alias grep='grep --color=auto'
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
-alias sc="nvim ~/.config/zsh/ && source ~/.config/zsh/.zshrc"
-alias ec="nvim ~/.config/nvim"

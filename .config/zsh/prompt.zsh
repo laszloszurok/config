f02caa51ce3cacb8548ac4896fb1b05171a889dc
László Szurok <szuroklaszlo97@gmail.com>
2020-12-07 15:35:53 +0100

    updated zsh prompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,21 +1,53 @@
-# Show git branch in right prompt if inside a git dir
+PROMPT_PREFIX="%Bcwd:%b "
+PROMPT_PREFIX_COLOR="%F{40}"
+
+CWD_COLOR="%F{5}"
+
+PROMPT_SYMBOL="%B%b "
+PROMPT_SYMBOL_COLOR="%F{40}"
+
+GIT_BRANCH_PREFIX="%Bon%b "
+GIT_BRANCH_PREFIX_COLOR="%F{40}"
+
+GIT_BRANCH_SYMBOL=" "
+GIT_BRANCH_SYMBOL_COLOR="%F{5}"
+
+# registering precmd function (executes every time before the prompt is drawn)
+# if cwd=$HOME --> display /home/username, else display ~/current/path 
+# usage: %v in PROMPT
+autoload -Uz add-zsh-hook
+print_cwd () {
+    if [ "$(pwd)" = "$HOME" ]; then
+        psvar[1]="$(pwd)"
+    else
+        psvar[1]="$(echo '~')${$(pwd)#$HOME} "
+    fi
+}
+add-zsh-hook precmd print_cwd
+
+# Show git branch in git directories
+
 setopt prompt_subst
 autoload -Uz vcs_info
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' stagedstr '%F{172}✚%f'
+zstyle ':vcs_info:*' unstagedstr '%F{172}●%f'
+
 zstyle ':vcs_info:*' actionformats \
-    '%F{5}[on branch:%F{3}-%F{5}%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
-zstyle ':vcs_info:*' formats       \
-    '%F{5}[%F{2}on %F{5} %F{2}%b%F{5}]%f '
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
-zstyle ':vcs_info:*' enable git cvs svn
+   '%F{5}%F{5}%b%F{5}%f %u %c (%a)'
+
+zstyle ':vcs_info:*' formats \
+   '%F{5}%F{5}%b%F{5}%f %u %c'
+
 vcs_info_wrapper() {
   vcs_info
   if [ -n "$vcs_info_msg_0_" ]; then
-    echo "%{$fg[grey]%}${vcs_info_msg_0_}%{$reset_color%}$del"
-  else
-    echo "  %{$fg[green]%}%m%{$reset_color%}    %{$fg[green]%}%n%{$reset_color%}"
+    echo "$GIT_BRANCH_PREFIX_COLOR$GIT_BRANCH_PREFIX%f$GIT_BRANCH_SYMBOL_COLOR$GIT_BRANCH_SYMBOL%f${vcs_info_msg_0_}%f$del"
   fi
 }
 
-RPROMPT=$'$(vcs_info_wrapper)'
-PS1="%{$fg[green]%}cwd: %{$fg[magenta]%}%~"$'\n'"%{$reset_color%} "
+# RPROMPT=$'$(vcs_info_wrapper)'
+PROMPT="$PROMPT_PREFIX_COLOR$PROMPT_PREFIX$CWD_COLOR%v"$'$(vcs_info_wrapper)'$'\n'"%f$PROMPT_SYMBOL_COLOR$PROMPT_SYMBOL%f"
 # RPS1="  %m    %n"

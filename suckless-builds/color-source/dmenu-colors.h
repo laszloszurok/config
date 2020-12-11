e3ba0f0feb2d8a59338062d68b4ebc655f9d3a99
László Szurok <szuroklaszlo97@gmail.com>
2020-12-11 16:38:23 +0100

    moved suckless builds to separate git repositories

--- suckless-builds/color-source/dmenu-colors.h
+++
@@ -1,8 +0,0 @@
-#include "variables.h"
-
-static const char *colors[SchemeLast][2] = {
-	/*                  fg         bg       */
-	[SchemeNorm] = { norm_fg, norm_bg },
-	[SchemeSel]  = { sel_fg, sel_bg },
-	[SchemeOut]  = { "#000000", "#00ffff" },
-};

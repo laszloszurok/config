87a07edaa2e40fa56c66eb5c6c4c0ec612c0ee03
László Szurok <szuroklaszlo97@gmail.com>
2020-12-13 10:32:09 +0100

    fixed cfg alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias la="ls -lAhF --group-directories-first"
 alias ls="ls --color=auto --group-directories-first"
 alias lh="ls -d .* --group-directories-first"
 alias llh="ls -ld .* --group-directories-first"
-alias cfg='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME' # alias for managing files in the home folder with git
+alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' # alias for managing files in the home folder with git
 alias ..='cd ..' 
 alias ...='cd ../..'
 alias grep='grep --color=auto'

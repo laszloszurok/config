d3c0d5142a88cb63e7d421e3e2ffebbc7ea95d08
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2021-06-04 16:10:35 +0200

    pressing escape in zsh menuselect mode quits the menu, pressing enete on a selection executes the command immediately

--- .config/zsh/autocomplete.zsh
+++ .config/zsh/autocomplete.zsh
@@ -16,4 +16,6 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect '^M' .accept-line
+bindkey -M menuselect '\e' accept-search
 bindkey -v '^?' backward-delete-char

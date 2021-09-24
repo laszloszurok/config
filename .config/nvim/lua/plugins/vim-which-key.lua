a5722ed7ed366ab24f39b47c5f47ac257bd33c65
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2021-09-24 12:43:02 +0200

    moved lsp keybindings into whickey

--- .config/nvim/lua/plugins/vim-which-key.lua
+++ .config/nvim/lua/plugins/vim-which-key.lua
@@ -62,7 +62,34 @@ local keymap = {
         name = '+jump',
         c = { "<cmd>lua require'hop'.hint_char1()<cr>", 'char' },
         l = { "<cmd>lua require'hop'.hint_lines()<cr>", 'line' },
-    }
+    },
+    l = {
+        name = '+lsp',
+        g = {
+            name = '+goto',
+            d = { '<cmd>lua vim.lsp.buf.declaration()<CR>', 'declaration' },
+            D = { '<cmd>lua vim.lsp.buf.definition()<CR>', 'definition' },
+            i = { '<cmd>lua vim.lsp.buf.implementation()<CR>', 'implementation' },
+            r = { '<cmd>lua vim.lsp.buf.references()<CR>', 'references' },
+            n = { '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', 'next diagnostic' },
+            p = { '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', 'prev diagnostic' },
+        },
+        d = { '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', 'diagnostics' },
+        f = { '<cmd>lua vim.lsp.buf.formatting()<CR>', 'format' },
+        r = { '<cmd>lua vim.lsp.buf.rename()<CR>', 'rename' },
+        h = { '<cmd>lua vim.lsp.buf.hover()<CR>', 'hover' },
+        H = { '<cmd>lua vim.lsp.buf.signature_help()<CR>', 'signature help' },
+        w = { 
+            name = '+workspace',
+            a = { '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', 'add folder' },
+            r = { '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', 'remove folder' },
+            l = { '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', 'list folders' },
+        },
+        a = { '<cmd>lua vim.lsp.buf.code_action()<CR>', 'code action' },
+        l = { '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', 'line diagnotics' },
+        t = { '<cmd>lua vim.lsp.buf.type_definition()<CR>', 'type definition' },
+        
+    },
 }
 
 wk.register_keymap('leader', keymap)

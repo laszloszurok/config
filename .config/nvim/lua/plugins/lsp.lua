5cb2d9ad754f2763426536ef09bb195a70b355ec
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2022-09-06 08:29:18 +0200

    add java language server to nvim lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,8 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+require'lspconfig'.java_language_server.setup{ cmd = { '/usr/share/java/java-language-server/lang_server_linux.sh' } }
+
 require'lspconfig'.bashls.setup{}
 
 require'lspconfig'.pyright.setup{}

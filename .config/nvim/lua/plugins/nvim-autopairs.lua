241301bdf7fa848aa3acbd3ea2f436b9fedc1748
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2022-09-10 14:16:20 +0200

    remove deprecated configuration from neovim autopairs plugin

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,16 +1,5 @@
 require('nvim-autopairs').setup{}
 
-require("cmp").setup({
-    map_cr = true,       --  map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-    auto_select = true,  -- automatically select the first item
-    insert = false,      -- use insert confirm behavior instead of replace
-    map_char = {         -- modifies the function or method delimiter by filetypes
-        all = '(',
-        tex = '{'
-    }
-})
-
 -- automatically add spaces between opening and closing parentheses
 local Rule = require 'nvim-autopairs.rule'
 require('nvim-autopairs').add_rules {
